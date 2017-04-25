bf9a1f6afca1fff3a3d2dd04ea6693f8187e50d9
Carlyse Jordan <carlyse.jordan@Carlyses-Air.lan>
2017-04-25 16:50:18 -0400

    created a function to retrievegroups in a category

---
+++ server/routes/group.route.js
@@ -0,0 +1,22 @@
+const groupRouter = require('express').Router();
+
+let fetch = require('node-fetch');
+let allGroups = [];
+
+groupRouter.get('/groups', function showAllGroups(req, res, next){
+  fetch('https://api.meetup.com/2/groups?category_id=34&key=6c3f627cb3159597874394e545dd5d',
+    {
+      method: 'GET'
+    }
+  ).then(function handleRes(resObj){
+    return resObj.json();
+  }).then(function resData(data){
+    return res.json(data);
+  }).catch(function handleErr(err){
+    console.log(err);
+    res.status(500);
+    res.json({ error:'unable to load data from the Meetup API' });
+  });
+});
+
+module.exports = groupRouter;
