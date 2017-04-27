a863ed860290a53bc7bce19b347abcad9257d007
Carlyse Jordan <carlyse.jordan@Carlyses-Air.lan>
2017-04-27 09:50:46 -0400

    added my states and ui-router

--- src/js/weekend.module.js
+++ src/js/weekend.module.js
@@ -1,4 +1,31 @@
 (function() {
   'use strict';
-   angular.module('weekend',[]);
+  angular.module('weekend',['ui-router']).config(routerConfig);
+
+  routerConfig.$inject = ['$stateProvider', 'urlRouterProvider'];
+
+  function routerConfig($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.when('', '/');
+
+    $urlRouterProvider.otherwise('/not-found');
+
+    $stateProvider
+    .state({
+      name: 'home',
+      url: '/',
+      templateUrl: 'src/index.html',
+      controller: 'homepageController',
+      controllerAs: 'hpCtrl'
+    }).state({
+      name: 'about',
+      url: '/about',
+      templateUrl: 'views/about.template.html'
+    }).state({
+      name: 'group',
+      url: '/groups',
+      templateUrl: 'views/group.template.html',
+      controller: 'groupController',
+      controllerAs: 'groupCtrl'
+    });
+  }
 }());
